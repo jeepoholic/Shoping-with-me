a4aa27d6a1a804a282f65d4929abd3bf0e692f81
Nabila Khan <94479534+jeepoholic@users.noreply.github.com>
2025-07-10 12:26:41 +0400

    innitial update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,38 @@
-import React from "react";
-import { communityLinks, platformLinks, resourcesLinks } from "../constants";
+
+import behanceIcon from "../assets/behance.svg";
+import linkedinIcon from "../assets/linkedin.svg";
+import  instagramIcon  from "../assets/instagram.svg";
+
 
 const Footer = () => {
   return (
-    <footer className="mt-20 border-t py-10 border-neutral-700 mx-20 xs:py-10">
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+<footer className="border-t py-4 border-neutral-700 xs:py-10 bg-gradient-to-r from-purple-900 to-purple-1000">
+<div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+  <div className="flex items-center justify-center">
+    <a href="https://www.behance.net/nabilakhan0125" target="_blank" rel="noopener noreferrer" className="flex items-center">
+      <img className="h-10 w-6 mr-2" src={behanceIcon} alt="logo" />
+      <span className="text-md font-semibold text-white">My Behance</span>
+    </a>
+  </div>
+  <div className="flex items-center justify-center">
+    <a href="https://www.linkedin.com/in/nabila-khan-b68ba0175/" target="_blank" rel="noopener noreferrer" className="flex items-center">
+      <img className="h-10 w-6 mr-2" src={linkedinIcon} alt="logo" />
+      <span className="text-md font-semibold text-white">My Linkedin</span>
+    </a>
+  </div>
+  <div className="flex items-center justify-center">
+    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="flex items-center">
+      <img className="h-10 w-6 mr-2" src={instagramIcon} alt="logo" />
+      <span className="text-md font-semibold text-white">My Instagram</span>
+    </a>
+  </div>
+</div>
+
+
+
+
+
+      {/* <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
         <div>
           <h3 className="text-md font-semibold mb-4 text-purple-800">Resources </h3>
           <ul className="space-y-2">
@@ -35,7 +63,7 @@ const Footer = () => {
             ))}
           </ul>
         </div>
-      </div>
+      </div> */}
     </footer>
   );
 };
